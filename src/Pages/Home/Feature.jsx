a3af6dd28bce38be07626845a41cb94624adecab
Sangram Bishwas <info.sangram2003@gmail.com>
2024-06-05 15:03:38 +0600

    implement a sample featured section with dami text and same images

---
+++ src/Pages/Home/Feature.jsx
@@ -0,0 +1,59 @@
+import SectionTitle from "../../SectionTitle/SectionTitle";
+import { Card } from "flowbite-react";
+import image from "../../assets/sliders-3.jpg";
+const Feature = () => {
+    return (
+        <div>
+            <SectionTitle
+                heading={"Our Feature"}
+                subHeading={"We are providing some great feature including"}></SectionTitle>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-5 md:px-10 lg:px-20 gap-y-8 lg:gap-y-16">
+                <Card className="max-w-sm space-y-4">
+                    <div className="flex flex-col items-center pb-10">
+                        <img className="rounded-full w-24 h-24" src={image} alt="" />
+                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Body Building</h5>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vitae ut iste culpa fuga voluptatem obcaecati eaque rerum eius nihil, commodi rem quasi dolorum nesciunt quia pariatur quibusdam dolores quisquam.</p>
+                    </div>
+                </Card>
+                <Card className="max-w-sm space-y-4">
+                    <div className="flex flex-col items-center pb-10">
+                        <img className="rounded-full w-24 h-24" src={image} alt="" />
+                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Body Building</h5>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vitae ut iste culpa fuga voluptatem obcaecati eaque rerum eius nihil, commodi rem quasi dolorum nesciunt quia pariatur quibusdam dolores quisquam.</p>
+                    </div>
+                </Card>
+                <Card className="max-w-sm space-y-4">
+                    <div className="flex flex-col items-center pb-10">
+                        <img className="rounded-full w-24 h-24" src={image} alt="" />
+                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Body Building</h5>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vitae ut iste culpa fuga voluptatem obcaecati eaque rerum eius nihil, commodi rem quasi dolorum nesciunt quia pariatur quibusdam dolores quisquam.</p>
+                    </div>
+                </Card>
+                <Card className="max-w-sm space-y-4">
+                    <div className="flex flex-col items-center pb-10">
+                        <img className="rounded-full w-24 h-24" src={image} alt="" />
+                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Body Building</h5>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vitae ut iste culpa fuga voluptatem obcaecati eaque rerum eius nihil, commodi rem quasi dolorum nesciunt quia pariatur quibusdam dolores quisquam.</p>
+                    </div>
+                </Card>
+                <Card className="max-w-sm space-y-4">
+                    <div className="flex flex-col items-center pb-10">
+                        <img className="rounded-full w-24 h-24" src={image} alt="" />
+                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Body Building</h5>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vitae ut iste culpa fuga voluptatem obcaecati eaque rerum eius nihil, commodi rem quasi dolorum nesciunt quia pariatur quibusdam dolores quisquam.</p>
+                    </div>
+                </Card>
+                <Card className="max-w-sm space-y-4">
+                    <div className="flex flex-col items-center pb-10">
+                        <img className="rounded-full w-24 h-24" src={image} alt="" />
+                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Body Building</h5>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vitae ut iste culpa fuga voluptatem obcaecati eaque rerum eius nihil, commodi rem quasi dolorum nesciunt quia pariatur quibusdam dolores quisquam.</p>
+                    </div>
+                </Card>
+            </div>
+
+        </div>
+    );
+};
+
+export default Feature;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Feature from "./Feature";
 import Slider from "./Slider";
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <Feature></Feature>
             this is homepages
         </div>
     );
